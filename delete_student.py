bba9e7cda24dbc3cbdaf981ca6868f99141d5771
JOE SHARP <jms@marling.school>
2020-10-02 08:17:01 +0000

    Added menu

--- delete_student.py
+++ delete_student.py
@@ -1,9 +1,5 @@
 from common import execute_sql, request_id
 
-SQL_DELETE_STUDIES = """
-  DELETE FROM studies WHERE student_id = ?
-"""
-
 SQL_DELETE_STUDENT = """
   DELETE FROM student WHERE id=?
 """
@@ -11,9 +7,6 @@ SQL_DELETE_STUDENT = """
 def delete_student():
   student_id = request_id('student')
   d = (student_id,)
-
-  # Clean up 'studies' first
-  execute_sql(SQL_DELETE_STUDIES, d)
 
   # Now we can safely delete the student
   execute_sql(SQL_DELETE_STUDENT, d)
--- main.py
+++ main.py
@@ -1,27 +1,18 @@
 from create_database import create_database
+from populate_database import populate_database
 from delete_database import delete_database
 from get_students import get_students
-from get_studies import get_studies
-from get_subjects import get_subjects
 from insert_student import insert_student
-from insert_studies import insert_studies
-from insert_subject import insert_subject
 from delete_student import delete_student
-from delete_subject import delete_subject
 from common import run_any_select, run_any_execute
 
 db_functions = [
   ('Create Database', create_database),
+  ('Populate Database', populate_database),
   ('Delete Database', delete_database),
   ('Get Students', get_students),
-  ('Get Studies', get_studies),
-  ('Get Subjects', get_subjects),
   ('Insert Student', insert_student),
-  ('Insert Studies', insert_studies),
-  ('Insert Subject', insert_subject),
-  ('Insert Subject', insert_subject),
   ('Delete Student', delete_student),
-  ('Delete Subject', delete_subject),
   ('Run SELECT SQL', run_any_select),
   ('Run INSERT/UPDATE/DELETE SQL', run_any_execute),
 ]
